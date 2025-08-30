474a9732ed6c83804092153ea56d0d8793414e6d
Ahmed Asim Zaman <ahmedasimzaman@srv966285.hstgr.cloud>
2025-08-30 08:44:50 +0000

    update

--- server.js
+++ server.js
@@ -15,9 +15,21 @@ app.set('trust proxy', 1);
 connectDB();
 
 // Security middleware
-app.use(helmet());
+
+const allowedOrigins = [
+  'https://crmama.com.mx',
+  'https://www.crmama.com.mx',
+'http://localhost:5173'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
@@ -84,7 +96,7 @@ app.use(require('./middleware/errorMiddleware'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📊 API Health: http://localhost:${PORT}/api/health`);
 });
